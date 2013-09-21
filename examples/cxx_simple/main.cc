196f78cfdec24a637c4bf14d994ac909a327591d
Matthew Brennan Jones <mattjones@workhorsy.org>
2013-09-20 21:29:35 -0700

    Fixed Bug #1225295: 'Add optimization and compile time build options'.

--- examples/cxx_simple/main.cc
+++ examples/cxx_simple/main.cc
@@ -18,8 +18,15 @@ class MathHelper {
 int main(int argc, char* argv[]) {
 	MathHelper math;
 
+	#ifdef ADD
 	int a = math.add(7, 9);
-	std::cout << "16 == " << a << std::endl;
+	std::cout << "7 + 9 == " << a << std::endl;
+	#endif
+
+	#ifdef MULTIPLY
+	int b = math.multiply(9, 12);
+	std::cout << "9 * 12 == " << b << std::endl;
+	#endif
 
 	return 0;
 }
