da1274ef44efa20b4c65cb5fedc913280b5e1da3
Matthew Brennan Jones <mattjones@workhorsy.org>
2014-02-13 14:36:08 -0800

    Cleanup of CPU module and new version of cpuinfo.py.

--- lib_raise/lib_raise_cpu.py
+++ lib_raise/lib_raise_cpu.py
@@ -25,14 +25,10 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import os
-import platform
-import multiprocessing
-import re
+import cpuinfo
 import lib_raise_helpers as Helpers
 import lib_raise_config as Config
 import lib_raise_users as Users
-import cpuinfo
 
 
 arch = None
@@ -55,123 +51,36 @@ def setup():
 	global cpus_total
 	global cpus_free
 
-	def set_arch(dirty_arch):
-		global arch
-		global bits
-		if re.match('^i\d86$|^x86$|^x86_32$|^i86pc$', dirty_arch):
-			arch = 'x86_32'
-			bits = '32'
-		elif re.match('^x86$|^x86_64$|^amd64$', dirty_arch):
-			arch = 'x86_64'
-			bits = '64'
-		else:
-			Config.early_exit('Unknown architecture {0}.'.format(dirty_arch))
+	info = cpuinfo.get_cpu_info()
 
-	# FIXME: Move all this into the cpuinfo.py library as a fallback for Windows
-	# For Windows get the CPU info from the register
-	if Helpers.os_type._name == 'Windows':
-		import _winreg
+	# Make sure to show an error if we can't get any CPU info
+	if not info:
+		Config.early_exit('Failed to get CPU info.')
 
-		# Get the CPU arch and bits
-		key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment")
-		dirty_arch = _winreg.QueryValueEx(key, "PROCESSOR_ARCHITECTURE")[0]
-		_winreg.CloseKey(key)
-		set_arch(dirty_arch)
+	# Make sure to show an error if we could not get the CPU arch or bits
+	if not info['processor_arch'] or not info['processor_bits']:
+		Config.early_exit('Unknown CPU architecture "{0}".'.format(info['processor_raw_arch_string']))
 
-		# Get the CPU MHz
-		key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, r"Hardware\Description\System\CentralProcessor\0")
-		mhz = _winreg.QueryValueEx(key, "~Mhz")[0]
-		_winreg.CloseKey(key)
+	# Get the CPU arch
+	arch = info['processor_arch']
 
-		# Get the CPU name
-		key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, r"Hardware\Description\System\CentralProcessor\0")
-		name = _winreg.QueryValueEx(key, "ProcessorNameString")[0]
-		_winreg.CloseKey(key)
+	# Get the CPU bits
+	bits = info['processor_bits']
 
-		# Get the CPU vendor name
-		key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, r"Hardware\Description\System\CentralProcessor\0")
-		vendor_name = _winreg.QueryValueEx(key, "VendorIdentifier")[0]
-		_winreg.CloseKey(key)
+	# Get the CPU MHz
+	mhz = info['processor_hz']
 
-		# Get the CPU features
-		key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, r"Hardware\Description\System\CentralProcessor\0")
-		feature_bits = _winreg.QueryValueEx(key, "FeatureSet")[0]
-		_winreg.CloseKey(key)
+	# Get the CPU name
+	name = info['processor_brand']
 
-		def is_set(bit):
-			mask = 0x80000000 >> bit
-			retval = mask & feature_bits > 0
-			return retval
+	# Get the CPU vendor name
+	vendor_name = info['vendor_id']
 
-		# http://en.wikipedia.org/wiki/CPUID
-		# http://unix.stackexchange.com/questions/43539/what-do-the-flags-in-proc-cpuinfo-mean
-		# http://www.lohninger.com/helpcsuite/public_constants_cpuid.htm
-		flags = {
-			'fpu' : is_set(0), # Floating Point Unit
-			'vme' : is_set(1), # V86 Mode Extensions
-			'de' : is_set(2), # Debug Extensions - I/O breakpoints supported
-			'pse' : is_set(3), # Page Size Extensions (4 MB pages supported)
-			'tsc' : is_set(4), # Time Stamp Counter and RDTSC instruction are available
-			'msr' : is_set(5), # Model Specific Registers
-			'pae' : is_set(6), # Physical Address Extensions (36 bit address, 2MB pages)
-			'mce' : is_set(7), # Machine Check Exception supported
-			'cx8' : is_set(8), # Compare Exchange Eight Byte instruction available
-			'apic' : is_set(9), # Local APIC present (multiprocessor operation support)
-			'sepamd' : is_set(10), # Fast system calls (AMD only)
-			'sep' : is_set(11), # Fast system calls
-			'mtrr' : is_set(12), # Memory Type Range Registers
-			'pge' : is_set(13), # Page Global Enable
-			'mca' : is_set(14), # Machine Check Architecture
-			'cmov' : is_set(15), # Conditional MOVe instructions
-			'pat' : is_set(16), # Page Attribute Table
-			'pse36' : is_set(17), # 36 bit Page Size Extensions
-			'serial' : is_set(18), # Processor Serial Number
-			'clflush' : is_set(19), # Cache Flush
-			#'reserved1' : is_set(20), # reserved
-			'dts' : is_set(21), # Debug Trace Store
-			'acpi' : is_set(22), # ACPI support
-			'mmx' : is_set(23), # MultiMedia Extensions
-			'fxsr' : is_set(24), # FXSAVE and FXRSTOR instructions
-			'sse' : is_set(25), # SSE instructions
-			'sse2' : is_set(26), # SSE2 (WNI) instructions
-			'ss' : is_set(27), # self snoop
-			#'reserved2' : is_set(28), # reserved
-			'tm' : is_set(29), # Automatic clock control
-			'ia64' : is_set(30), # IA64 instructions
-			'3dnow' : is_set(31) # 3DNow! instructions available
-		}
-
-		# Get a list of only the flags that are true
-		flags = [k for k, v in flags.items() if v]
-		flags.sort()
-	# For everything else, assume unix/linux
-	else:
-		# First try /proc/cpuinfo
-		info = cpuinfo.get_cpu_info_from_proc_cpuinfo()
-
-		# If not, try querying the CPU cpuid register
-		if not info:
-			info = cpuinfo.get_cpu_info_from_cpuid()
-		if not info:
-			raise Exception('Failed to get CPU info.')
-
-		# Get the CPU arch and bits
-		set_arch(platform.machine())
-
-		# Get the CPU MHz
-		mhz = info['processor_hz']
-
-		# Get the CPU name
-		name = info['processor_brand']
-
-		# Get the CPU vendor name
-		vendor_name = info['vendor_id']
-
-		# Get the CPU features
-		flags = info['flags']
+	# Get the CPU features
+	flags = info['flags']
 
 	# Figure out how many cpus there are
-	cpus_total = multiprocessing.cpu_count()
+	cpus_total = info['processor_count']
 	cpus_free = cpus_total
 
 setup()
